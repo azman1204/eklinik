2f78b43fea9700de5909ee16e0710b245a1fdc7f
Jimmy <azman740412@gmail.com>
2019-04-09 12:39:36 +0800

    form zone

--- resources/views/admin/zoneform.blade.php
+++ resources/views/admin/zoneform.blade.php
@@ -5,17 +5,19 @@ Zon IKM
 @endsection
 
 @section('content')
-
-<div class="row">
-    <div class="col-md-1">Kod</div>
-    <div class="col-md-4"><input type="text" class="form-control"></div>
-</div>
-<div class="row">
-    <div class="col-md-1">Nama IKM</div>
-    <div class="col-md-4"><input type="text" class="form-control"></div>
-</div>
-<div class="row">
-    <div class="col-md-1"></div>
-    <div class="col-md-4"><input type="submit" class="btn btn-primary"></div>
+<div class="col-md-2"></div>
+<div class="col-md-6">
+    <div class="row">
+        <div class="col-md-2">Kod</div>
+        <div class="col-md-10"><input type="text" class="form-control"></div>
+    </div>
+    <div class="row">
+        <div class="col-md-2">Nama IKM</div>
+        <div class="col-md-10"><input type="text" class="form-control"></div>
+    </div>
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10"><input type="submit" class="btn btn-primary"></div>
+    </div>
 </div>
 @endsection
